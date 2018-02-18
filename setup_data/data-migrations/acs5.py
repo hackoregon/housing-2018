ed46f576ab86681df97c3e8de88949fff7e2d700
Alec Peters <apeters@gatewaytocollege.org>
2018-02-17 16:18:58 -0800

    added migration for acs 5-year profile data, reading from all data files in setup_data/data-migrations/data/acs5 folder

---
+++ setup_data/data-migrations/acs5.py
@@ -0,0 +1,137 @@
+import os
+from datetime import datetime
+
+import pytz
+from pytz import timezone
+import pandas as pd
+import numpy as np
+import requests
+from json import JSONDecodeError
+import elasticsearch
+import elasticsearch.helpers
+
+def get_url(year, survey, variables, geography, location=None, is_profile=False):
+    base_url = 'https://api.census.gov/data'
+    # for some reason url changes in these cases
+    if (survey == 'acs1' and year >= 2015) or (survey == 'acs5' and year >= 2016) or (survey == 'acs5' and is_profile and year in [2016,2015,2011]):
+        survey = 'acs/' + survey
+    profile = '/profile' if is_profile else ''
+    url = f'{base_url}/{year}/{survey}{profile}?get={variables}&for={geography}&key={API_KEY}'
+    if location is not None:
+        url += f'&in={location}'
+    return url
+
+def get_results(url):
+    response = requests.get(url)
+    try:
+        return response.json()
+    except JSONDecodeError:
+        return response.text
+
+def get_variable_df(year, profile=False):
+    if profile and year in [2016,2015,2011]:
+        year = '{}/acs'.format(year)
+    elif not profile and year >= 2016:
+        year = '{}/acs'.format(year)
+    variables_url = 'https://api.census.gov/data/{}/acs5{}/variables.json'.format(year, '/profile' if profile else '')
+    resp = requests.get(variables_url)
+    variables = resp.json()
+    var_list = []
+    for k, v in variables['variables'].items():
+        try:
+            v['id'] = k
+            var_list.append(v)
+        except:
+            continue
+    var_df = pd.DataFrame(var_list)
+    #var_df.set_index('id', drop=False, inplace=True)
+    return var_df
+
+def split_filename(filename):
+    parts = filename.split('_')
+    return '_'.join(parts[:2]), parts[2], parts[3]
+    
+def get_file_definition(filename):
+    data_id, state_id, year = split_filename(filename)
+    row = profile_estimates.loc[data_id]
+    return row
+
+if __name__ == '__main__':
+    API_KEY = '46828e32fefc67853bb60cd34eeb89e9db5cdcbd'
+    DATA_DIR = '/usr/src/app/data-migrations'
+    ES_URL = 'http://elastic:{}@elasticsearch:9200/'.format(os.environ['ELASTIC_PASSWORD'])
+    INDEX = 'acs5'
+
+    pacific = timezone('US/Pacific')
+    date_fmt = "%Y-%m-%dT%H:%M:%S.000%z"
+    es = elasticsearch.Elasticsearch([ES_URL])
+
+    # downloaded from https://www.census.gov/geographies/reference-files.All.html
+    geographies = pd.read_excel('https://www2.census.gov/programs-surveys/popest/geographies/2016/all-geocodes-v2016.xlsx', header=4)
+    geographies.set_index(['Summary Level', 'State Code (FIPS)', 'County Code (FIPS)'], inplace=True, drop=False)
+    geographies.sort_index(inplace=True)
+
+    profile_vrs = get_variable_df(2016, profile=True)
+    profile_ids = profile_vrs['id'].values
+    for yr in range(2015, 2010, -1):
+        df = get_variable_df(yr, profile=True)
+        profile_ids = np.intersect1d(profile_ids, df['id'].values)
+    profile_vrs = profile_vrs[profile_vrs['id'].isin(profile_ids)].set_index('id', drop=False).sort_index()
+    profile_estimates = profile_vrs.loc[profile_vrs.index.map(lambda x: x.endswith('E'))]
+    profile_estimates = profile_estimates[profile_estimates['group']!='DP02PR']
+
+    # Create index
+    try:
+        insert_ct = 0
+        es.indices.create(index=INDEX, ignore=400)
+
+        for file in os.listdir(DATA_DIR + '/data/acs5'):
+            if 'profile' not in file:
+                continue
+            census_id, state_id, year = split_filename(file)
+            info = get_file_definition(file)
+            labels = info.label.split('!!')
+            data = pd.read_csv(DATA_DIR + '/data/acs5/' + file)
+            data['state_name'] = geographies.at[(40, int(state_id), 0), 'Area Name (including legal/statistical area description)'][0]
+            data['county_name'] = data.apply(lambda x: geographies.at[(50, x.state, x.county), 'Area Name (including legal/statistical area description)'][0], axis=1)
+            
+            def get_doc():
+                for row in data.iterrows():
+                    d = row[1]
+                    action = { '_index': INDEX, '_type': 'default'}
+                    body = {
+                            'time': datetime(int(year), 1, 1, tzinfo=pytz.utc).astimezone(pacific).strftime(date_fmt),
+                            'source': 'profile',
+                            'census_id': d.index[0], 
+                            'state_name': d['state_name'], 
+                            'state_id': d['state'],
+                            'county_name': d['county_name'],
+                            'county_id': d['county'],
+                            'concept': info.concept,
+                            'valuetype': labels[0],
+                            'datatype': labels[1],
+                            'group': labels[2] if len(labels) > 2 else '',
+                            'value': d[0]
+                    }
+                    action['_source'] = body
+                    yield action
+                    
+            def delete_items():
+                query = {"query": {"bool": {"filter": [{ "term": { "census_id": census_id }}, { "term": { "state_id": state_id }}, { "term": { "time": datetime(int(year), 1, 1, tzinfo=pytz.utc).astimezone(pacific).strftime(date_fmt) }}]}}}
+                for e in elasticsearch.helpers.scan(es, index=INDEX, doc_type='default', scroll='1m', query=query, _source=None):
+                    # There exists a parameter to avoid this del statement (`track_source`) but at my version it does    n't exists.
+                    del e['_score']
+                    e['_op_type'] = 'delete'
+                    yield e
+
+            elasticsearch.helpers.bulk(es, actions=delete_items())
+            result = elasticsearch.helpers.bulk(es, actions=get_doc())
+            insert_ct += result[0]
+                
+            break
+
+        requests.post(ES_URL + 'datasets/default/', json={ 'ran': True, 'lastUpdate': datetime.now().strftime(date_fmt), 'title': 'acs5', 'description': 'American Community Survey 5-Year', 'source': 'https://api.census.gov/data/2016/acs/acs5/profile/variables.html', 'message': 'Success. {} documents added.'.format(insert_ct) })
+
+    except BaseException as e:
+        es.indices.delete(INDEX)
+        requests.post(ES_URL + 'datasets/default/', json={ 'ran': False, 'lastUpdate': datetime.now().strftime(date_fmt), 'title': 'acs5', 'description': 'American Community Survey 5-Year', 'source': 'https://api.census.gov/data/2016/acs/acs5/profile/variables.html', 'message': str(e) })
