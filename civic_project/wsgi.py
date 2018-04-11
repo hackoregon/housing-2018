7fc747beb404e04aaa8a0da5b7907ef725572bac
Alec Peters <apeters@gatewaytocollege.org>
2018-04-11 15:55:20 -0700

    ran create-project.sh to start new project titled civic_project

--- civic_project/wsgi.py
+++ civic_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for dead_songs project.
+WSGI config for civic_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dead_songs.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "civic_project.settings")
 
 application = get_wsgi_application()
