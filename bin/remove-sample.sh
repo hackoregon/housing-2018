dfeef090a73027031e4e5a7463c65a40805ea554
Alec Peters <apeters@gatewaytocollege.org>
2018-04-13 13:48:54 -0700

    added database removal to remove sample script

--- bin/remove-sample.sh
+++ bin/remove-sample.sh
@@ -2,8 +2,15 @@
 
 set -eou pipefail
 
+# Grab environment variables
+. ./.env
+
 if [ -f manage.py ]; then rm manage.py; fi
 rm -rf dead_songs
 rm -rf api
 
 if [ -f ./Backups/dead_songs.sql ]; then rm ./Backups/dead_songs.sql; fi
+
+# remove sample database
+docker-compose -f development-docker-compose.yml run api_development /bin/bash -c "export PGPASSWORD=${DEVELOPMENT_POSTGRES_PASSWORD}; psql -h "$DEVELOPMENT_POSTGRES_HOST" -U "$DEVELOPMENT_POSTGRES_USER" -c 'DROP DATABASE IF EXISTS dead_songs'; unset PGPASSWORD"
+
