04bef7ec52a3daeb104cbbdc99e8123a4272a97c
Alec Peters <apeters@gatewaytocollege.org>
2018-04-13 13:50:04 -0700

    moved create-app and create-project into single file

--- bin/create-api-project.sh
+++ bin/create-api-project.sh
@@ -2,14 +2,16 @@
 
 set -euo pipefail
 
-if [ -z "${PROJECT_NAME:-}" ]; then
-   . bin/set-env.sh 
-fi
+# Grab environment variables
+. ./.env
 
+# Remove sample files and database
 bin/remove-sample.sh
 
+# Create django project and api app
 docker-compose -f development-docker-compose.yml run api_development /bin/bash -c "django-admin.py startproject $PROJECT_NAME . ; ./manage.py startapp api"
 
+# Replace newly created settings with the example settings
 rm $PROJECT_NAME/settings.py
 cp ./bin/example-settings.py $PROJECT_NAME/settings.py
 
