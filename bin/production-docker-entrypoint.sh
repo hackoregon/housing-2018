74dcc466db42feb608f415258a550f8a29c7ac62
Alec Peters <apeters@gatewaytocollege.org>
2018-04-18 10:25:29 -0700

    updated scripts with new production terminology

--- bin/production-docker-entrypoint.sh
+++ bin/production-docker-entrypoint.sh
@@ -5,17 +5,15 @@
 
 set -eou pipefail
 
-export PGPASSWORD=$POSTGRES_PASSWORD
-until psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -p "$POSTGRES_PORT" -d "$POSTGRES_NAME" -c '\q'
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -p "$POSTGRES_PORT" -d "$POSTGRES_NAME" -c '\q'
 do
   >&2 echo "Postgres is unavailable - sleeping"
-  sleep 15
+  sleep 5
 done
 
 >&2 echo "Postgres is up"
 
 echo Debug: $DEBUG
-
 
 ./manage.py collectstatic --noinput
 
