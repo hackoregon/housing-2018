239a50775b5fd10ae5fac65a5aae0221cc548147
Alec Peters <apeters@gatewaytocollege.org>
2018-04-30 23:09:41 -0700

    added models for hud data

--- api/models.py
+++ api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 from autoslug import AutoSlugField
 
 class JCHSData(models.Model):
@@ -13,3 +14,29 @@ class JCHSData(models.Model):
     datapoint_clean = AutoSlugField(populate_from='datapoint', max_length=100)
     datatype_clean = AutoSlugField(populate_from='datatype', max_length=100)
     valuetype_clean = AutoSlugField(populate_from='valuetype', max_length=100)
+
+class HudPitData(models.Model):
+    datapoint = models.CharField(max_length=255, help_text='Location of data')
+    geography = models.CharField(max_length=255, help_text='Location type')
+    datatype = models.CharField(max_length=255, help_text='Description of the data this value represents')
+    year = models.PositiveSmallIntegerField(help_text='Year of count')
+    value = models.DecimalField(max_digits=18, decimal_places=2)
+
+    # URL-friendly values to access via GET requests
+    datapoint_clean = AutoSlugField(populate_from='datapoint', max_length=100)
+    datatype_clean = AutoSlugField(populate_from='datatype', max_length=100)
+
+class HudHicData(models.Model):
+    datapoint = models.CharField(max_length=255, help_text='Location of data')
+    geography = models.CharField(max_length=255, help_text='Location type')
+    datatype = models.CharField(max_length=255, help_text='Description of the data this value represents')
+    shelter_status = ArrayField(
+        models.CharField(max_length=255, help_text='Type of shelter this value represents')
+    )
+    year = models.PositiveSmallIntegerField(help_text='Year of count')
+    value = models.DecimalField(max_digits=18, decimal_places=2)
+    
+    # URL-friendly values to access via GET requests
+    datapoint_clean = AutoSlugField(populate_from='datapoint', max_length=100)
+    datatype_clean = AutoSlugField(populate_from='datatype', max_length=100)
+    
