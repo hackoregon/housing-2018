5d352f5fb82251118ad6840886318e42959fa3e3
Alec Peters <apeters@gatewaytocollege.org>
2018-05-23 14:55:46 -0700

    added loader for policy data

--- data/loader.py
+++ data/loader.py
@@ -1,7 +1,9 @@
 from data.loaders.jchs_data_2017 import load_data as load_jchs_data
 from data.loaders.hud_homelessness import load_data as load_hud_data
 from data.loaders.urbaninstitute_rentalcrisis import load_data as load_urbaninstitute_data
+from data.loaders.policy_inventory import load_data as load_policy_data
 
-load_jchs_data()
-load_hud_data()
-load_urbaninstitute_data()
+#load_jchs_data()
+#load_hud_data()
+#load_urbaninstitute_data()
+load_policy_data()
