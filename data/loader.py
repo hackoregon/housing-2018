e5c844d3198ca1be054180391067329a8f094ca6
Alec Peters <apeters@gatewaytocollege.org>
2018-05-23 16:43:24 -0700

    uncommented other data loaders

--- data/loader.py
+++ data/loader.py
@@ -3,7 +3,7 @@ from data.loaders.hud_homelessness import load_data as load_hud_data
 from data.loaders.urbaninstitute_rentalcrisis import load_data as load_urbaninstitute_data
 from data.loaders.policy_inventory import load_data as load_policy_data
 
-#load_jchs_data()
-#load_hud_data()
-#load_urbaninstitute_data()
+load_jchs_data()
+load_hud_data()
+load_urbaninstitute_data()
 load_policy_data()
