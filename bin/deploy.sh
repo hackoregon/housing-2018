44dc60d38e446ccb03ced8117abbf75f9d9c638b
Alec Peters <apeters@gatewaytocollege.org>
2018-06-04 12:58:24 -0700

    updated to match changes from PR #30

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -9,18 +9,25 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     echo Getting the ECR login...
     eval $(aws ecr get-login --region $AWS_DEFAULT_REGION --no-include-email)
 
-    REMOTE_DOCKER_PATH="$DOCKER_REPO"/"$DOCKER_REPO_NAMESPACE"/"$PRODUCTION_DOCKER_IMAGE"
+    REMOTE_DOCKER_PATH="$DOCKER_REPO"/"$DOCKER_REPO_NAMESPACE"/"$DOCKER_IMAGE"
     TAG=travis-buildnum-"$TRAVIS_BUILD_NUMBER"
     echo Tagging with "$TAG"
-    docker tag "$PRODUCTION_DOCKER_IMAGE":latest "$REMOTE_DOCKER_PATH":"$TAG"
+    docker tag "$DOCKER_IMAGE":latest "$REMOTE_DOCKER_PATH":"$TAG"
     echo Running docker push command...
     docker push "$REMOTE_DOCKER_PATH":"$TAG"
-#    echo Running ecs-deploy.sh script...
-#    ./bin/ecs-deploy.sh  \
-#     -n "$ECS_SERVICE_NAME" \
-#     -c "$ECS_CLUSTER"   \
-#     -i "$REMOTE_DOCKER_PATH":"$TAG" \
-#     --timeout 300
+
+    TAG=latest
+    echo Tagging with "$TAG"
+    docker tag "$DOCKER_IMAGE":latest "$REMOTE_DOCKER_PATH":"$TAG"
+    echo Running docker push command...
+    docker push "$REMOTE_DOCKER_PATH":"$TAG"
+
+    echo Running ecs-deploy.sh script...
+    ./bin/ecs-deploy.sh  \
+     -n "$ECS_SERVICE_NAME" \
+     -c "$ECS_CLUSTER"   \
+     -i "$REMOTE_DOCKER_PATH":latest \
+     --timeout 300
    else
      echo "Skipping deploy because branch is not master"
   fi
