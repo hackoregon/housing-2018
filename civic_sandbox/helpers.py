dee41aa95a87d1ada88c8cbe893e5cbcee4ce785
Ara <araoshinsky@gmail.com>
2018-06-21 09:57:40 -0700

    update sandbox dates

--- civic_sandbox/helpers.py
+++ civic_sandbox/helpers.py
@@ -18,6 +18,10 @@ def sandbox_view_factory(model_class, serializer_class, multi_geom_class, geom_f
                 variable_column = dates['date_attribute']
                 filter = variable_column + '__contains'
                 dataset= model_class.objects.filter(**{ filter: date_filter })
+                if settings.DEBUG: print('---------------------------------------')
+                if settings.DEBUG: print(filter)
+                if settings.DEBUG: print(date_filter)
+
 
             if settings.DEBUG: print('made queryset')
         
@@ -25,39 +29,27 @@ def sandbox_view_factory(model_class, serializer_class, multi_geom_class, geom_f
             coords = []
             for each in dataset: 
                 geom = getattr(each, geom_field)
+                if geom is not None:
                 ## converts MultiPolygons to Polygons ##
-                if isinstance(geom, MultiPolygon):
-                    coords.append(geom.convex_hull)
-
-                else: 
-                    coords.append(geom)
-            
+                  if isinstance(geom, MultiPolygon):
+                      coords.append(geom.convex_hull)
+                  else: 
+                      coords.append(geom)
+              
             multi = multi_geom_class(coords)
             limit_boundary = multi.convex_hull.json
 
             if settings.DEBUG: print('boundary calculation complete')
-
-            # calculate date meta #
             
-            min_date = None
-            max_date = None
-            if dates['date_attribute'] is not None: 
-                min_date = min(dates['date_attribute'])
-                max_date = max(['date_attribute'])
-    
         except model_class.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+            return Response(status.HTTP_404_NOT_FOUND)
 
         serializer = serializer_class(dataset, many=True)
 
         response= { 
             'slide_meta': {
                 'boundary': [json.loads(limit_boundary)],
-                'dates': {
-                    'min_date': min_date, 
-                    'max_date': max_date, 
-                    'date_granularity': dates['date_granularity'],
-                },
+                'dates': dates,
                 'attributes': attributes
                 },
             'slide_data': serializer.data
@@ -66,4 +58,3 @@ def sandbox_view_factory(model_class, serializer_class, multi_geom_class, geom_f
         return Response(response)
 
     return sandbox_function
-
